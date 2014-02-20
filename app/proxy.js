e670d6d2319a8274cd162958e8cde61b5ca5841d
Michael Johnston <mikej165@gmail.com>
2014-02-20 08:45:44 -0500

    Add simply API request proxy.

--- app/proxy.js
+++ app/proxy.js
@@ -1,23 +1,59 @@
 var http = require('http');
-var sys  = require('sys');
+var child_process = require('child_process');
+var fs = require('fs');
 
-http.createServer(function(request, response) {
-    sys.log(request.connection.remoteAddress + ": " + request.method + " " + request.url);
-    var proxy = http.createClient(6800, request.headers['host'])
-    var proxy_request = proxy.request(request.method, request.url, request.headers);
-    proxy_request.addListener('response', function (proxy_response) {
-        proxy_response.addListener('data', function(chunk) {
-            response.write(chunk, 'binary');
+http.createServer(onRequest).listen(3000);
+
+var json_cmds = ['addversion.json','schedule.json','cancel.json','listprojects.json','listversions,json','listspiders.json','listjobs.json','delversion.json','delproject.json'];
+
+function isApiRequest(url){
+    var result = false;
+    url = url.toLowerCase();
+    for(var i = 0; i < json_cmds.length; i++){
+        if(url.indexOf(json_cmds[i]) != -1){
+            console.log('Found: ' + json_cmds[i]);
+            result = true;
+            break;
+        }
+    }
+    return result;
+}
+
+
+function onRequest(client_req, client_res) {
+
+    var options = {
+        hostname: 'ec2-50-16-60-252.compute-1.amazonaws.com',
+        port: 6800,
+        path: client_req.url,
+        method: 'GET'
+    };
+
+    if(isApiRequest(client_req.url) === true){
+        // Proxy all API requests
+
+        var proxy = http.request(options, function (res) {
+            res.pipe(client_res, {
+                end: true
+            });
         });
-        proxy_response.addListener('end', function() {
-            response.end();
+        client_req.pipe(proxy, {
+            end: true
         });
-        response.writeHead(proxy_response.statusCode, proxy_response.headers);
-    });
-    request.addListener('data', function(chunk) {
-        proxy_request.write(chunk, 'binary');
-    });
-    request.addListener('end', function() {
-        proxy_request.end();
-    });
-}).listen(8080);
+    } else {
+
+        var f = client_req.url.substring(1);
+
+        // Serve up ordinary files from the local filesystem
+        var readStream = fs.createReadStream(f);
+        // This will wait until we know the readable stream is actually valid before piping
+        readStream.on('open', function () {
+            // This just pipes the read stream to the response object (which goes to the client)
+            readStream.pipe(client_res);
+        });
+
+    }
+}
+
+
+child_process.spawn('open', ['http://localhost:3000/index.html']);
